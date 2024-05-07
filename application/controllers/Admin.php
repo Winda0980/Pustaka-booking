d4480b335beeac5e00a731de396b3b2a328ce856
Winda O Nababan <wnababan44@gmail.com>
2024-05-07 13:50:00 +0700

    Pertemuan 6

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -21,7 +21,5 @@ class Admin extends CI_Controller
         $this->load->view('templates/topbar', $data);
         $this->load->view('admin/index', $data);
         $this->load->view('templates/footer');
-
-        
     }
 }
