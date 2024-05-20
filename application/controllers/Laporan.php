8e21963a1f4a1e33b3e93a0cf20a2ed02277e7d8
Winda O Nababan <wnababan44@gmail.com>
2024-05-20 22:28:38 +0700

    Pertemuan 9

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -28,4 +28,41 @@ class Laporan extends CI_Controller
 
         $this->load->view('buku/laporan_print_buku', $data);
     }
+    public function laporan_buku_pdf()
+    {
+        // Load data from ModelBuku
+        $data['buku'] = $this->ModelBuku->getBuku()->result_array();
+
+        // Load Dompdf library
+        $sroot = $_SERVER['DOCUMENT_ROOT'];
+        include $sroot . "/pustaka-booking1/application/third_party/dompdf/autoload.inc.php";
+
+        // Initialize Dompdf
+        $dompdf = new Dompdf\Dompdf();
+
+        // Load the view and pass the data
+        $this->load->view('buku/laporan_pdf_buku', $data);
+
+        // Get the output from the view and set paper size and orientation
+        $html = $this->output->get_output();
+        $paper_size = 'A4';
+        $orientation = 'landscape';
+        $dompdf->set_paper($paper_size, $orientation);
+
+        // Convert HTML to PDF
+        $dompdf->load_html($html);
+        $dompdf->render();
+
+        // Stream the generated PDF to the browser
+        $dompdf->stream("laporan_data_buku.pdf", array('Attachment' => 0));
+    }
+    public function export_excel()
+    {
+        $data = array(
+            'title' => 'Laporan Buku',
+            'buku' => $this->ModelBuku->getBuku()->result_array()
+        );
+        $this->load->view('buku/export_excel_buku', $data);
+    }
+
 }
